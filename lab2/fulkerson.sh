805aa24a3d1b094bca9e5a6c46f34eed956f9277
daniil.kachura <daniil.kachura@innowise-group.com>
2023-03-26 13:39:54 +0300

    fulkerson2

--- lab2/fulkerson.sh
+++ lab2/fulkerson.sh
@@ -2,3 +2,12 @@
 declare -A network=(
 # Тут я вношу свои вершины, к примеру [s,1]=0 [s,2]=6
 )
+
+function find_augmenting_path {
+  local parent=()
+  local queue=()
+  local visited=()
+
+  queue+=(s)
+  visited+=(s)
+  parent[s]=-1
